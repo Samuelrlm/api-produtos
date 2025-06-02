6b87e76bc7a2352e84e830098853472f882c9400
Samuel Lima <105258182+Samuelrlm@users.noreply.github.com>
2025-06-02 12:33:36 -0300

    add

--- index.js
+++ index.js
@@ -1,50 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 6579
-const produtosDb = []
-const usersDb = []
+const port = 6579;
+const produtosRoutes = require('./src/routes/produtos')
 
 app.use(express.json())
-
-app.get("/", (req, res) => {
-    res.send("Olá Samuel!")
-})
-
-app.post("/produtos", (req, res) => {
-    const {nome, preco, categoria} = req.body
-
-    if(!nome || !preco || !categoria){
-        return res.status(400).send("nome, preco e categoria são obrigatorios")
-    }
-
-
-    const produto = {
-        nome: nome,
-        preco: preco,
-        categoria: categoria
-    }
-
-    produtosDb.push(produto)
-
-    res.status(201).send(produto)
-})
-
-app.get("/produtos", (req, res) => {
-    res.send(produtosDb)
-})
-
-app.post("/usuarios", (req, res) => {
-    const {nome, email, senha} = req.body
-
-    if(!nome || !email || !senha){
-        return res.status(400).send("email, nome e senha são obrigatorios")
-    }
-
-    usersDb.push({nome, email, senha})
-
-    res.status(201).send("Usuário cadastrado com sucesso!")
-})
+app.use(produtosRoutes)
 
 app.listen(port, () => {
-    console.log(`O servidor está rodando na porta ${port}`)
+    console.log(`Servidor rodando na porta ${port}`)
 })
